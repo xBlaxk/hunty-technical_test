ac8e3eb5dca23f0481440f9c89b3322837001b5b
Blaxk <blaxkmaster@gmail.com>
2023-06-17 21:07:53 -0500

    test: navigate to every playground link and verify URL and Title

--- automation/cypress/e2e/main.cy.js
+++ automation/cypress/e2e/main.cy.js
@@ -1,12 +1,13 @@
-/// <reference types="Cypress" />
 import NavBarPage from '../pageObject/navBar/navBar.po'
 import homePage from '../pageObject/home/home.po'
 import homeData from '../pageObject/home/home.json'
 
+let fixtureData = {}
 describe('template spec', () => {
     beforeEach('Navigate to page', () => {
         cy.fixture('testData').then((data) => {
             cy.visit(`${data.homePage}/`)
+            fixtureData = data
         })
     })
 
@@ -19,20 +20,192 @@ describe('template spec', () => {
     it('Verify home page title and images', () => {
         homePage.getPageTitle().should('have.text', homeData.pageTitle)
 
-        homePage.getMainImage()
+        homePage
+            .getMainImage()
             .invoke('attr', 'src')
             .should('equal', homeData.imageSrc)
         homePage.getImageLicense().contains(homeData.imageLicense)
-        homePage.getImageLicense()
+        homePage
+            .getImageLicense()
             .find('a')
             .then((element) => {
-                cy.wrap(element[0]).invoke('attr', 'href').should('equal', homeData.rubiksCubeImageUrl)
-                cy.wrap(element[1]).invoke('attr', 'href').should('equal', homeData.rubiksCubeLicenseUrl)
+                cy.wrap(element[0])
+                    .invoke('attr', 'href')
+                    .should('equal', homeData.rubiksCubeImageUrl)
+                cy.wrap(element[1])
+                    .invoke('attr', 'href')
+                    .should('equal', homeData.rubiksCubeLicenseUrl)
             })
 
         homePage.getAlertMessage().should('have.text', homeData.alertMessage)
 
-        homePage.getCitationMessage().should('have.text', homeData.citationMessage)
-        homePage.getCitationAuthor().should('have.text', homeData.citationAuthor)
+        homePage
+            .getCitationMessage()
+            .should('have.text', homeData.citationMessage)
+        homePage
+            .getCitationAuthor()
+            .should('have.text', homeData.citationAuthor)
+    })
+
+    it('Access Dynamic ID Page, verify url and title', () => {
+        homePage.getDynamicIdLink().then((link) => {
+            const linkText = link.text()
+            cy.wrap(link).click();
+            cy.url().should('include', fixtureData.dynamicIdUrl)
+            cy.title().should('eq', linkText)
+        })
+    })
+
+    it('Access Class Attribute Page, verify url and title', () => {
+        homePage.getClassAttributeLink().then((link) => {
+            const linkText = link.text()
+            cy.wrap(link).click();
+            cy.url().should('include', fixtureData.classAttrUrl)
+            cy.title().should('eq', linkText)
+        })
+    })
+
+    it('Access Hidden Layers Page, verify url and title', () => {
+        homePage.getHiddenLayersLink().then((link) => {
+            const linkText = link.text()
+            cy.wrap(link).click();
+            cy.url().should('include', fixtureData.hiddenLayersUrl)
+            cy.title().should('eq', linkText)
+        })
+    })
+
+    it('Access Load Delay Page, verify url and title', () => {
+        homePage.getLoadDelayLink().then((link) => {
+            const linkText = link.text()
+            cy.wrap(link).click();
+            cy.url().should('include', fixtureData.loadDelayUrl)
+            cy.title().should('eq', `${linkText}s`)
+        })
+    })
+
+    it('Access Ajax Data Page, verify url and title', () => {
+        homePage.getAjaxDataLink().then((link) => {
+            const linkText = link.text()
+            cy.wrap(link).click();
+            cy.url().should('include', fixtureData.ajaxDataUrl)
+            cy.title().should('eq', linkText)
+        })
+    })
+
+    it('Access Client Delay Page, verify url and title', () => {
+        homePage.getClientDelayLink().then((link) => {
+            const linkText = link.text()
+            cy.wrap(link).click();
+            cy.url().should('include', fixtureData.clientDelayUrl)
+            cy.title().should('eq', linkText)
+        })
+    })
+
+    it('Access Click Page, verify url and title', () => {
+        homePage.getClickLink().then((link) => {
+            const linkText = link.text()
+            cy.wrap(link).click();
+            cy.url().should('include', fixtureData.clickUrl)
+            cy.title().should('eq', linkText)
+        })
+    })
+
+    it('Access Text Input Page, verify url and title', () => {
+        homePage.getTextInputLink().then((link) => {
+            const linkText = link.text()
+            cy.wrap(link).click();
+            cy.url().should('include', fixtureData.textInputUrl)
+            cy.title().should('eq', linkText)
+        })
+    })
+
+    it('Access Scrollbars Page, verify url and title', () => {
+        homePage.getScrollbarsLink().then((link) => {
+            const linkText = link.text()
+            cy.wrap(link).click();
+            cy.url().should('include', fixtureData.scrollbarsUrl)
+            cy.title().should('eq', linkText)
+        })
+    })
+
+    it('Access Dynamic Table Page, verify url and title', () => {
+        homePage.getDynamicTableLink().then((link) => {
+            const linkText = link.text()
+            cy.wrap(link).click();
+            cy.url().should('include', fixtureData.dynamicTableUrl)
+            cy.title().should('eq', linkText)
+        })
+    })
+
+    it('Access Verify Text Page, verify url and title', () => {
+        homePage.getVerifyTextLink().then((link) => {
+            const linkText = link.text()
+            cy.wrap(link).click();
+            cy.url().should('include', fixtureData.verifyTextUrl)
+            cy.title().should('eq', linkText)
+        })
+    })
+
+    it('Access Progress Bar Page, verify url and title', () => {
+        homePage.getProgressbarLink().then((link) => {
+            const linkText = link.text()
+            cy.wrap(link).click();
+            cy.url().should('include', fixtureData.progressbarUrl)
+            cy.title().should('eq', linkText)
+        })
+    })
+
+    it('Access Visibility Page, verify url and title', () => {
+        homePage.getVisibilityLink().then((link) => {
+            const linkText = link.text()
+            cy.wrap(link).click();
+            cy.url().should('include', fixtureData.visibilityUrl)
+            cy.title().should('eq', linkText)
+        })
+    })
+
+    it('Access Sample App Page, verify url and title', () => {
+        homePage.getSampleAppLink().then((link) => {
+            const linkText = link.text()
+            cy.wrap(link).click();
+            cy.url().should('include', fixtureData.sampleAppUrl)
+            cy.title().should('eq', linkText)
+        })
+    })
+
+    it('Access Mouse Over Page, verify url and title', () => {
+        homePage.getMouseOverLink().then((link) => {
+            const linkText = link.text()
+            cy.wrap(link).click();
+            cy.url().should('include', fixtureData.mouseOverUrl)
+            cy.title().should('eq', linkText)
+        })
+    })
+
+    it('Access Non-breaking Space Page, verify url and title', () => {
+        homePage.getNonBreakingSpaceLink().then((link) => {
+            const linkText = link.text()
+            cy.wrap(link).click();
+            cy.url().should('include', fixtureData.nonBreakingSpaceUrl)
+            cy.title().should('eq', linkText)
+        })
+    })
+
+    it('Access Overlapping Elements Page, verify url and title', () => {
+        homePage.getOverlappedElementLink().then((link) => {
+            const linkText = link.text()
+            cy.wrap(link).click();
+            cy.url().should('include', fixtureData.overlappedUrl)
+            cy.title().should('eq', linkText)
+        })
+    })
+
+    it('Access Shadow DOM Page, verify url and title', () => {
+        homePage.getShadowDOMLink().then((link) => {
+            const linkText = link.text()
+            cy.wrap(link).click();
+            cy.url().should('include', fixtureData.shadowDOMUrl)
+            cy.title().should('eq', linkText)
+        })
     })
 })
--- automation/cypress/pageObject/home/home.po.js
+++ automation/cypress/pageObject/home/home.po.js
@@ -28,6 +28,87 @@ class Home {
         return cy.get('#citation > footer')
     }
 
+    /**
+     *   PLAYGROUND LINK
+     */
+    static getDynamicIdLink() {
+        return cy.get('a[href="/dynamicid"]')
+    }
+
+    static getClassAttributeLink() {
+        return cy.get('a[href="/classattr"]')
+    }
+
+    static getHiddenLayersLink() {
+        return cy.get('a[href="/hiddenlayers"]')
+    }
+
+    static getLoadDelayLink() {
+        return cy.get('a[href="/loaddelay"]')
+    }
+
+    static getAjaxDataLink() {
+        return cy.get('a[href="/ajax"]')
+    }
+
+    static getClientDelayLink() {
+        return cy.get('a[href="/clientdelay"]')
+    }
+
+    static getClickLink() {
+        return cy.get('a[href="/click"]')
+    }
+
+    static getTextInputLink() {
+        return cy.get('a[href="/textinput"]')
+    }
+
+    static getScrollbarsLink() {
+        return cy.get('a[href="/scrollbars"]')
+    }
+
+    static getDynamicTableLink() {
+        return cy.get('a[href="/dynamictable"]')
+    }
+
+    static getVerifyTextLink() {
+        return cy.get('a[href="/verifytext"]')
+    }
+
+    static getProgressbarLink() {
+        return cy.get('a[href="/progressbar"]')
+    }
+
+    static getVisibilityLink() {
+        return cy.get('a[href="/visibility"]')
+    }
+
+    static getSampleAppLink() {
+        return cy.get('a[href="/sampleapp"]')
+    }
+
+    static getMouseOverLink() {
+        return cy.get('a[href="/mouseover"]');
+    }
+
+    static getNonBreakingSpaceLink() {
+        return cy.get('a[href="/nbsp"]');
+    }
+
+    static getOverlappedElementLink() {
+        return cy.get('a[href="/overlapped"]');
+    }
+
+    static getShadowDOMLink() {
+        return cy.get('a[href="/shadowdom"]');
+    }
+
+    /**
+     *  VERIFY METHODS
+     */
+    static verifyPageTitle(expectedTitle) {
+        cy.title().should('eq', expectedTitle);
+    }
 }
 
 export default Home
